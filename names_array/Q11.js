02db369df930d3b335b11c6f78f61ccf4ea01782
Iqra Asad <160169965+iqramalk@users.noreply.github.com>
2024-03-12 23:30:26 +0500

    Add files via upload
    use of array and for.. of loop to understand the method of writing hoe can we print each name on different lines.

---
+++ names_array/Q11.js
@@ -0,0 +1,7 @@
+//Q11 represting friends names while using array 
+var names = ["Kissa", "Ruman", "Maryam", "Maliha"];
+//Here I used "for...of" loops instead of "for loop" to make that code clean and more readable
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var i = names_1[_i];
+    console.log(i);
+}
---
+++ names_array/Q11.ts
@@ -0,0 +1,6 @@
+//Q11 represting friends names while using array 
+let names:string[]= ["Kissa","Ruman","Maryam","Maliha"];
+//Here I used "for...of" loops instead of "for loop" to make that code clean and more readable
+for (const i of names) {
+    console.log(i);
+}
