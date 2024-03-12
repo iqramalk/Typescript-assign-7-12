39bfeaee8255113538a84d46b9c79989ec8c3ae4
Iqra Asad <160169965+iqramalk@users.noreply.github.com>
2024-03-12 23:07:24 +0500

    Add files via upload
    About how can we take variables and distribute them in each operation.

---
+++ maths_4lines/Q8.js
@@ -0,0 +1,4 @@
+console.log(5 + 3);
+console.log(14 - 6);
+console.log(8 * 1);
+console.log(16 / 2);
---
+++ maths_4lines/Q8.ts
@@ -0,0 +1,6 @@
+console.log(5+3); //Addition
+console.log(14-6); //Subraction
+console.log(8*1); //multiplication
+console.log(16/2); //division
+
+
