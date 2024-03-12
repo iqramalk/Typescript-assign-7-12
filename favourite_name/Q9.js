0633ca01136a5264d8d88e4132285761245a39f3
Iqra Asad <160169965+iqramalk@users.noreply.github.com>
2024-03-12 23:11:35 +0500

    Add files via upload
    Showing favorite number.

---
+++ favourite_name/Q9.js
@@ -0,0 +1,4 @@
+// Q9 storing favourite number in a message
+var fav_number = 7;
+var message = "My favorite number is ".concat(fav_number, ".");
+console.log(message);
---
+++ favourite_name/Q9.ts
@@ -0,0 +1,5 @@
+// Q9 storing favourite number in a message
+let fav_number: number = 7;
+let message: string = `My favorite number is ${fav_number}.`;
+console.log(message);
+
