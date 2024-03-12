f529ff84f0577fed20e0fb9b9ac2d4239db153eb
Iqra Asad <160169965+iqramalk@users.noreply.github.com>
2024-03-12 23:28:28 +0500

    Add files via upload
    Implementation of comments makes easy for us to understand each line purpose.

---
+++ comment_usage/Q10.js
@@ -0,0 +1,12 @@
+//Q10 Adding Comments
+// Name = Iqra
+// Date: March 10, 2024
+// This TypeScript program demonstrates basic string manipulation functions like toUpperCase(),
+// toLowerCase(), and replace().
+var message = "Hello, Everyone I am a Content writer.";
+// Convert the string to uppercase
+var upperCaseMessage = message.toUpperCase();
+console.log("Uppercase message: ".concat(upperCaseMessage));
+// Convert the string to lowercase
+var lowerCaseMessage = message.toLowerCase();
+console.log("Lowercase message: ".concat(lowerCaseMessage));
---
+++ comment_usage/Q10.ts
@@ -0,0 +1,17 @@
+//Q10 Adding Comments
+// Name = Iqra
+// Date: March 10, 2024
+
+// This TypeScript program demonstrates basic string manipulation functions like toUpperCase(),
+// toLowerCase(), and replace().
+
+const message = "Hello, Everyone I am a Content writer.";
+
+// Convert the string to uppercase
+const upperCaseMessage = message.toUpperCase();
+console.log(`Uppercase message: ${upperCaseMessage}`);
+
+// Convert the string to lowercase
+const lowerCaseMessage = message.toLowerCase();
+console.log(`Lowercase message: ${lowerCaseMessage}`);
+
