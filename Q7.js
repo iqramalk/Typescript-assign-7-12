be2d86f4fae5470b2033f8f0971eec341705e505
Iqra Asad <160169965+iqramalk@users.noreply.github.com>
2024-03-12 23:05:08 +0500

    Add files via upload
    About using different Arithmetic Operations.

---
+++ Q7.js
@@ -0,0 +1,5 @@
+var n1 = 2;
+var n2 = 4;
+var n3 = 16;
+var n4 = 10;
+console.log(n2 + n2, n4 - n1, n3 / n1, n2 * n1);
---
+++ Q7.ts
@@ -0,0 +1,6 @@
+// Q7 mathematical operation to get 8 
+let n1= 2;
+let n2= 4;
+let n3= 16;
+let n4= 10;
+console.log(n2+n2, n4-n1, n3/n1, n2*n1);
